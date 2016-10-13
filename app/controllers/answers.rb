9feb16ec8d6fc285a68be311d1586cf36939366f
Greg Garner & Kyle Dorsey <pair+garner412.kylepdorsey@devbootcamp.com>
2016-10-13 15:13:45 -0500

    add ability to answer questions and handle blank content errors

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -4,7 +4,17 @@ get '/questions/:question_id/answers/new' do
 end
 
 post '/questions/:question_id/answers' do
-  if logged_in?
+  # if logged_in?
+    @question = Question.find_by(id: params[:question_id])
+    answer = Answer.new(content: params[:content], question_id: params[:question_id], user_id: 1)
+    if answer.save
+      p @question
+      redirect "/questions/#{@question.id}"
+    else
+      @errors = answer.errors.full_messages
+      erb :'answers/new'
+    end
+  # end
 end
 
 put '/questions/:question_id/answers/:answer_id' do
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,4 +4,6 @@ class Answer < ActiveRecord::Base
   belongs_to :question
   has_many :comments, as: :commentable
   has_many :votes, as: :voteable
+
+  validates :content, presence: true
 end
