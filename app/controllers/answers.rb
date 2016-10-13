31453339465afa2588813760a4b610f1e96c3a02
Greg Garner & Kyle Dorsey <pair+garner412.kylepdorsey@devbootcamp.com>
2016-10-13 15:34:55 -0500

    change button

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -4,7 +4,7 @@ get '/questions/:question_id/answers/new' do
 end
 
 post '/questions/:question_id/answers' do
-  # if logged_in?
+  if logged_in?
     @question = Question.find_by(id: params[:question_id])
     answer = Answer.new(content: params[:content], question_id: params[:question_id], user_id: 1)
     if answer.save
@@ -14,7 +14,7 @@ post '/questions/:question_id/answers' do
       @errors = answer.errors.full_messages
       erb :'answers/new'
     end
-  # end
+  end
 end
 
 put '/questions/:question_id/answers/:answer_id' do
