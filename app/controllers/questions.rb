7e364311f6db7bfa6ffc921b3ce018c46820d1e2
Greg Garner & Kyle Dorsey <pair+garner412.kylepdorsey@devbootcamp.com>
2016-10-13 11:57:45 -0500

    create questions controller and basic routes

---
+++ app/controllers/questions.rb
@@ -0,0 +1,27 @@
+get '/questions' do
+  @questions = Question.all
+  erb :'questions/index'
+end
+
+post '/questions' do
+  # @question = Question.new()
+  # if @question.save
+
+  # else
+
+  # end
+end
+
+get 'questions/:id' do
+  @question = Question.find_by(id: params[:id])
+  erb :'questions/show'
+end
+
+put 'questions/:id' do
+  @question = Question.find_by(id: params[:id])
+end
+
+delete 'questions/:id' do
+  # @question = Question.find_by(id: params[:id])
+
+end
