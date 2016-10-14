85608648663adcf7bcfc009a815d6dedfd24b45d
Greg Garner & Kyle Dorsey <pair+garner412.kylepdorsey@devbootcamp.com>
2016-10-14 12:21:42 -0500

    fix ajax login and stuff.

--- public/js/application.js
+++ public/js/application.js
@@ -9,5 +9,33 @@ $(document).ready(function() {
       $('#login-register').html(response);
       $('#login-register').show();
     })
+    .fail(function(response){
+      console.log(resposne)
+    })
+  })
+  $("#login-register").on("submit", "#login-form", function(event){
+    event.preventDefault();
+
+    var $form = $(this);
+    var url = $form.attr('action');
+    var type = $form.attr('method');
+    var data = $form.serialize();
+
+    $.ajax({
+      url: url,
+      type: type,
+      data: data
+    })
+    .done(function(response){
+      window.location.replace(response)
+
+    })
+    .fail(function(response){
+      console.log(response)
+      $("#login-errors").text("")
+      $("#login-errors").append(response.responseText)
+    })
   })
 });
+
+
