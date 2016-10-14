28f59ef186bd2ad25894cdb2a82529d7b9634391
Henry Fyfe & Jonathan Warshaw <pair+hfyfe.jwarshaw@devbootcamp.com>
2016-10-14 11:33:17 -0500

    add editing of questions make question form partial ensure only question owner can edit or see edit link

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -13,6 +13,9 @@ post '/questions' do
 end
 
 get '/questions/new' do
+  @question = Question.new
+  @method = "Post"
+  @action = "/questions/"
   if logged_in?
     erb :'/questions/new'
   else
@@ -25,12 +28,29 @@ get '/questions/:id' do
   erb :'questions/show'
 end
 
+get '/questions/:id/edit' do
+  @question = Question.find_by(id: params[:id])
+  @method = "Put"
+  @action = "/questions/#{@question.id}"
+  if owner?(@question.user)
+    erb :'questions/edit'
+  else
+    redirect "/questions/#{params[:id]}"
+  end
+end
+
 put '/questions/:id' do
-  # @question = Question.find_by(id: params[:id])
+  @question = Question.find_by(id: params[:id])
+  @question.update_attributes(params[:question])
+  if @question.persisted?
+    redirect "/questions/#{params[:id]}"
+  else
+    @errors = @question.errors.full_messages
+    erb :'questions/edit'
+  end
 end
 
 delete '/questions/:id' do
   # @question = Question.find_by(id: params[:id])
-
 end
 
